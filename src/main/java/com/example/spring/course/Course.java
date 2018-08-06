317aa0f8389a59195d8af45d6f43cfac06d1f9d6
DMC\shafisha <shahabshafiee1981@gmail.com>
2018-08-06 11:33:31 +0200

    uncomment course classes

--- src/main/java/com/example/spring/course/Course.java
+++ src/main/java/com/example/spring/course/Course.java
@@ -1,57 +1,62 @@
-//package com.example.spring.course;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-////import com.example.topic.Topic;
-//
-//
-//
-//@Entity
-//public class Course {
-//	
-//	@Id
-//	private String id;
-//	private String name;
-//	private String description;
-//	
-////	@Autowired
-////	private Topic topic;
-//	
-//	public Course() {
-//	}
-//	
-////	public Course(String id, String name, String description, String topicId) {
+package com.example.spring.course;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.spring.topic.Topic;
+
+//import com.example.topic.Topic;
+
+
+
+@Entity
+public class Course {
+	
+	@Id
+	private String id;
+	private String name;
+	private String description;
+	
+//	@Autowired
+	private Topic topic;
+	
+	public Course() {
+	}
+	
+	public Course(String id, String name, String description, String topicId) {
 //	public Course(String id, String name, String description) {
-//		this.id = id;
-//		this.name = name;
-//		this.description = description;
-////		this.topic=new Topic(topicId, "", "");
-//	}
-//	public String getId() {
-//		return id;
-//	}
-//	public void setId(String id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getDescription() {
-//		return description;
-//	}
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-////	public Topic getTopic() {
-////		return topic;
-////	}
-////	public void setTopic(Topic topic) {
-////		this.topic = topic;
-////	}
-//}
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.setTopic(new Topic(topicId, "", ""));
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+//	/
+
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+}
--- src/main/java/com/example/spring/course/CourseRepository.java
+++ src/main/java/com/example/spring/course/CourseRepository.java
@@ -1,7 +1,7 @@
-//package com.example.spring.course;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface CourseRepository extends CrudRepository<Course, String>{
-//
-//}
+package com.example.spring.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String>{
+
+}
